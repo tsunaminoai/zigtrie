4c86f2f05f83d0d366f16fb52d4050121b3dae63
TsunamiNoAi <spam@falseblue.com>
2024-06-02 19:25:20 -0400

    tries!

--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,45 @@
 const std = @import("std");
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+pub fn Trie(comptime T: type) type {
+    const TrieNode = struct {
+        children: std.ArrayList(TN),
+        value: T,
+
+        const TN = @This();
+
+        pub fn init(alloc: std.mem.Allocator, value: T) !*TN {
+            const tn = try alloc.create(TN);
+            tn.* = .{
+                .value = value,
+                .children = std.ArrayList(TN).init(alloc),
+            };
+            return tn;
+        }
+    };
+    return struct {
+        arena: std.heap.ArenaAllocator,
+        root: *TrieNode,
+
+        const Self = @This();
+
+        pub fn init(alloc: std.mem.Allocator) !Self {
+            var arena = std.heap.ArenaAllocator.init(alloc);
+            const child_alloc = arena.allocator();
+
+            return Self{
+                .root = try TrieNode.init(child_alloc, 'X'),
+                .arena = arena,
+            };
+        }
+        pub fn deinit(self: Self) void {
+            self.arena.deinit();
+        }
+    };
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+test "Trie()" {
+    const alloc = testing.allocator;
+    var t = try Trie(u8).init(alloc);
+    defer t.deinit();
 }
